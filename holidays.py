e677a83e8d25c6493bcfb2df309b59a3582b2842
DEEPAKp17 <50896621+DEEPAKp17@users.noreply.github.com>
2019-05-29 22:58:49 +0530

    Create holidays.py

---
+++ holidays.py
@@ -0,0 +1,7 @@
+a=input("")
+if(a=="Monday" or a=="Tuesday" or a=="Wednesday" or a=="Thursday" or a=="Friday"):
+ print("no")
+elif(a=="Saturday" or a=="Sunday"):
+ print("yes")
+else:
+ print("invalid")
